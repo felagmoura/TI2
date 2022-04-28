75a38e8f15d9964602a8e5009569bbb813c92371
Felipe Moura <86014843+felagmoura@users.noreply.github.com>
2022-04-28 14:09:30 -0300

    Add files via upload

---
+++ Integracao PostgreSQL/src/main/java/ex02/DAO.java
@@ -0,0 +1,58 @@
+package ex02;
+
+import java.sql.*;
+import java.security.*;
+import java.math.*;
+
+public class DAO {
+	
+	protected Connection conexao;
+	
+	public DAO() {
+		conexao = null;
+	}
+	
+	public boolean conectar() {
+		
+		String driverName = "org.postgresql.Driver";                    
+		String serverName = "localhost";
+		String mydatabase = "teste";
+		int porta = 5432;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String username = "postgres";
+		String password = "0M!ssiss!M0";
+		boolean status = false;
+		
+		try {
+			Class.forName(driverName);
+			conexao = DriverManager.getConnection(url, username, password);
+			status = (conexao == null);
+			System.out.println("Conexão efetuada com o postgres!"); 
+		} catch (ClassNotFoundException e) {
+			System.err.println("Conexão deu ruim class");
+		} catch (SQLException e) {
+			System.err.println("Conexão deu ruim sql");
+		}
+		
+		return status;
+	}
+	
+	public boolean close() {
+		boolean status = false;
+		
+		try {
+			conexao.close();
+			status = true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return status;
+	}
+	
+	public static String toMD5(String senha) throws Exception {
+		MessageDigest m=MessageDigest.getInstance("MD5");
+		m.update(senha.getBytes(),0, senha.length());
+		return new BigInteger(1,m.digest()).toString(16);
+	}
+}
