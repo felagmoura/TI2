54867579d9e87fc2e52084c4a348f436d5e2639f
Felipe Moura <86014843+felagmoura@users.noreply.github.com>
2022-04-28 14:49:29 -0300

    Update DAO.java

--- Integracao PostgreSQL/src/main/java/ex02/DAO.java
+++ Integracao PostgreSQL/src/main/java/ex02/DAO.java
@@ -20,7 +20,7 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "postgres";
-		String password = "0M!ssiss!M0";
+		String password = "senha";
 		boolean status = false;
 		
 		try {
